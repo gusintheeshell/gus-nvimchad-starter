4b658f3b829ea457ad3d1924b37d3a96cd578a51
Gustavo Barbosa <gusbdev@gmail.com>
2024-11-02 16:46:13 -0300

    feat: enhance conform.lua with additional formatters and options

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,33 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        json = { "prettierd", "prettier", stop_after_first = true },
+        markdown = { "prettierd", "prettier", stop_after_first = true },
+        yaml = { "prettierd", "prettier", stop_after_first = true },
+    },
+    
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+
+    default_format_opts = {
+        lsp_format = "fallback",
+    },
+
+    formatters = {
+        shfmt = {
+            prepend_args = { '-i', '2' }
+        },
+    },
 }
 
 return options
