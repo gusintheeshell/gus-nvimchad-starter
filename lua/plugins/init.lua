592aef348c5cbb94de4380b1af77ee546a5e51a5
Gustavo Barbosa <gusbdev@gmail.com>
2024-11-02 16:47:03 -0300

    feat: enhance plugin configuration with event and command options

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,61 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        opts = require "configs.conform",
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("configs.lualine").setup({})
+        end,
+    },
+
+    {
+        "windwp/nvim-ts-autotag",
+        event = "VeryLazy",
+        opts = {},
+    },
+
+    {
+        "github/copilot.vim",
+        cmd = "Copilot",
+        event = { "VeryLazy" },
+        config = function()
+            require "configs.copilot"
+        end,
+    },
+
+    {
+    	"nvim-treesitter/nvim-treesitter",
+    	config = function()
+    		require "configs.treesitter"
+    	end,
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+        config = function()
+            require("configs.lualine").setup({})
+        end,
+    },
+
+    {
+        "AndreM222/copilot-lualine",
+        after = "lualine.nvim",
+        config = function()
+            require("copilot-lualine").setup()
+        end,
+    }
 }
